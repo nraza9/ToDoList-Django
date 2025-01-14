1ba8f9119f29acb2af51ca8ba31647ea3072eece
Nasir Raza <raza.nasir.lex@gmail.com>
2025-01-13 19:37:55 -0500

    Did some cleanup code and added .gitignore

--- app/api/views.py
+++ app/api/views.py
@@ -1,18 +1,11 @@
-from rest_framework.viewsets import ModelViewSet
 from ..models import Task
 from .serializers import AppSerializer
-
-from django.shortcuts import render
 from rest_framework.views import APIView
-#from . models import *
 from rest_framework.response import Response
-#from . serializer import *
 from rest_framework import status
 
-# class AppViewSet(ModelViewSet):
-#     queryset = Task.objects.all()
-#     serializer_class = AppSerializer
-    
+# Request handers
+
 class AppViewSet(APIView):
   
     serializer_class = AppSerializer
@@ -24,7 +17,6 @@ class AppViewSet(APIView):
         return Response(detail)
 
     def post(self, request):
-        print('=== post')
         serializer = AppSerializer(data=request.data)
         if serializer.is_valid(raise_exception=True):
             serializer.save()
@@ -32,15 +24,13 @@ class AppViewSet(APIView):
     
     # DELETE method for deleting a task by ID
     def delete(self, request, task_id=None):
-        print("=== delete: " + str(task_id))
-        #print("=== request: " + str(request.data['id']))
         try:
             task = Task.objects.get(id=task_id)
             task.delete()
             return Response({"message": "Task deleted successfully."}, status=status.HTTP_204_NO_CONTENT)
         except Task.DoesNotExist:
             return Response({"error": "Task not found."}, status=status.HTTP_404_NOT_FOUND)
-        
+
+    #ToDo: add code    
     def patch(self, request, task_id=None):
-        print("=== patch: " )
         return  Response(request.data)
