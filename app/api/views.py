32839fa3da5aeca2481feda8d4e70fb4828d8b41
Nasir Raza <raza.nasir.lex@gmail.com>
2025-01-14 12:52:39 -0500

    Added support for Patch method

--- app/api/views.py
+++ app/api/views.py
@@ -3,14 +3,24 @@ from .serializers import AppSerializer
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
+from django.shortcuts import get_object_or_404
 
 # REST API request handers
-
 class AppViewSet(APIView):
   
     serializer_class = AppSerializer
 
-    def get(self, request):
+    def get(self, request, task_id=None):
+        # Get the single task
+        if task_id != None:
+            task = get_object_or_404(Task, pk=task_id)
+            data = {
+                'id': task.id,
+                'title': task.title,
+            }
+            return Response(data)
+        
+        # Return all tasks
         # Data to be returned
         detail = [ {"id": detail.id, "title": detail.title, "created": detail.created_at} 
         for detail in Task.objects.all()]
@@ -31,6 +41,15 @@ class AppViewSet(APIView):
         except Task.DoesNotExist:
             return Response({"error": "Task not found."}, status=status.HTTP_404_NOT_FOUND)
 
-    #ToDo: add code    
+    # Update the Task's title   
     def patch(self, request, task_id=None):
-        return  Response(request.data)
+        try:
+            item = Task.objects.get(pk=task_id)
+        except Task.DoesNotExist:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+
+        serializer = AppSerializer(item, data=request.data, partial=True)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
